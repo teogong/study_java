c89097e4c6efe331ac58e316a7a823121ae07095
BIT <BIT@DESKTOP-ABDQ8A7>
2023-02-07 20:15:31 +0900

    part5

---
+++ JavaStudyProject2/src/com/teogong/part5/GarbageObjectExample.java
@@ -0,0 +1,7 @@
+package com.teogong.part5;
+
+public class GarbageObjectExample {
+	public static void main(String[] args) {
+		Sting hobby = "여행"
+	}
+}
---
+++ JavaStudyProject2/src/com/teogong/part5/NullPointerExceptionExample.java
@@ -0,0 +1,12 @@
+package com.teogong.part5;
+
+public class NullPointerExceptionExample {
+	public static void main(String[] args) {
+		int[] intArray = null;
+		//intArray[0] = 10;
+		
+		String str = null;
+		//System.out.println("총 문자 수: " + str.lenght());
+	}
+
+}
