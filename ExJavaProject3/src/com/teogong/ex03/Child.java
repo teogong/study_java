e78d15e2881bdd8aa61d6eb6d71bd1590f3a30f0
BIT <BIT@DESKTOP-ABDQ8A7>
2023-02-08 21:31:23 +0900

    연습문제 및 복습

---
+++ ExJavaProject3/src/com/teogong/ex03/Child.java
@@ -0,0 +1,21 @@
+package com.teogong.ex03;
+
+public class Child extends Parent{
+	public int studentNo;
+
+	public Child(String name, int studentNo) {
+		super(name);
+		this.studentNo = studentNo;
+	}
+	
+	
+	
+	
+//	public Child(String name, int studentNo) {
+//		this.name = name;
+//		this.studentNo = studentNo;
+//	}
+
+	
+	
+}
---
+++ ExJavaProject3/src/com/teogong/ex03/Machine.java
@@ -0,0 +1,7 @@
+package com.teogong.ex03;
+
+public abstract class Machine {
+	public void powerOn() {}
+	public void powerOff() {}
+	public abstract void work();
+}
